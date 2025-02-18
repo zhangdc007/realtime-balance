fdb4a104ea6e07e1126e5235cbf7a5e19f95e9c1
ZhangDaochuan <zhangdc007@users.noreply.github.com>
2025-02-18 09:06:43 +0800

    Update TransactionRepository.java
    add 1000 limit in retry schdual task

--- src/main/java/com/mybank/balance/transaction/dao/TransactionRepository.java
+++ src/main/java/com/mybank/balance/transaction/dao/TransactionRepository.java
@@ -17,6 +17,6 @@ import java.util.List;
 public interface TransactionRepository extends R2dbcRepository<Transaction, Long> {
     Mono<Transaction> findByBizId(String bizId);
 
-//    @Query("SELECT * FROM transactions WHERE status = 'RETRY' OR (status IN (:statuses) AND updated_at < :threshold)")
-    Flux<Transaction> findAllByStatusOrStatusInAndUpdatedAtBefore(String status,@Param("statuses") List<String> statuses, @Param("threshold") LocalDateTime threshold);
+//    @Query("SELECT * FROM transactions WHERE status = 'RETRY' OR (status IN (:statuses) AND updated_at < :threshold limit 1000)")
+    Flux<Transaction> findTop1000ByStatusOrStatusInAndUpdatedAtBefore(String status,@Param("statuses") List<String> statuses, @Param("threshold") LocalDateTime threshold);
 }
